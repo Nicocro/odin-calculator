9ef02e8bcc1ce7615dd117b4450a6ef80a89f00e
nicocro <ncroce00@gmail.com>
2024-03-10 16:27:16 +0000

    some basic functions added

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 
+let firstN; 
+let operator;
+let secondN; 
+
+// create the functions to populate the display 
+// when I start clicking values buttons//
+
+const digitButtons = document.querySelectorAll('.digit');
+digitButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const display = document.querySelector('.display');
+        display.textContent += button.textContent;
+    });
+});
+
+
+function operate(a, b, op){
+    switch (op) {
+        case '+' :
+            return add(a,b);
+        case '-' :
+            return subtract(a,b);
+        case '*' :
+            return multiply(a,b);
+        case '/' :
+            return divide(a,b); 
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -12,6 +41,6 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a % b 
+    return a / b 
     // need to handle the case where b = 0 here 
 }
