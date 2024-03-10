fccb43aeaef542574ac3f996a547ee82a1ea3ef1
nicocro <ncroce00@gmail.com>
2024-03-10 15:48:45 +0000

    basics skeleton is there

---
+++ app.js
@@ -0,0 +1,17 @@
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a % b 
+    // need to handle the case where b = 0 here 
+}
